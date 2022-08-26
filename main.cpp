34820ba6da9dd6e07f6578e22dc562ba0b294e36
delphox60 <delphox60@gmail.com>
2022-08-26 16:24:13 +0900

    feat: 행/열 사이즈 입력 기능 구현

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r_size;
+    int c_size;
+
+    cout << "Please enter number of columns: ";
+    cin >> c_size;
+
+    cout << "Please enter number of rows: ";
+    cin >> r_size;
+}
